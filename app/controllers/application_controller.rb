91b39dfc98cffb30bb5541decbb0b7b194150702
looh16 <langacustodio@gmail.com>
2022-11-27 17:47:38 +0200

    Add controllers and authentication configurations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :entities, foreign_key: 'author_id', dependent: :destroy
   has_many :groups, foreign_key: 'author_id', dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 50, minimum: 3 }
+  # validates :name, presence: true, length: { maximum: 50, minimum: 3 }
 end
