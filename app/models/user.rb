406e6b6fbc59a700d72b70f21379ee54d006e86a
looh16 <langacustodio@gmail.com>
2022-11-28 16:01:10 +0200

    Add tests for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :entities, foreign_key: 'author_id', dependent: :destroy
   has_many :groups, foreign_key: 'author_id', dependent: :destroy
 
-  # validates :name, presence: true, length: { maximum: 50, minimum: 3 }
+  validates :name, presence: true
 end
