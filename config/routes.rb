bc948aa852a05bfb46886959b797f79139cd42d8
looh16 <langacustodio@gmail.com>
2022-11-30 14:44:51 +0200

    Add styles

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: 'groups#index'
 
   devise_for :users
+  
 
   resources :users
+  resources :splash, only: [:index]
   resources :groups, only: [:index, :new, :create] do
     resources :entities, only: [:index, :new, :create]
   end
