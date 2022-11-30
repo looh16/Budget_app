90d40d7c13d510f997e4efcd1a1ab7a391bcacae
looh16 <langacustodio@gmail.com>
2022-11-30 19:08:03 +0200

    Add welcome view and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  root to: 'groups#index'
+ # root to: 'groups#index'
 
   devise_for :users
+
+  devise_scope :user do
+    authenticated do
+      root to: 'groups#index'
+    end
+
+    unauthenticated do
+      root to: 'welcomes#index', as: 'unauthenticated_user_root'
+    end
+    
+  end
   
 
   resources :users
